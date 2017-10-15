0d275ada56e5ec87623db22d7afa3c6e3228b130
Maryna Usatiuk <maryna.duda@gmail.com>
2017-10-15 16:42:33 +0300

    ADD animation route Add animation packege and clases to components

--- tasks/files-inject.js
+++ tasks/files-inject.js
@@ -1,13 +1,15 @@
 module.exports = {
     "js": [
         "./node_modules/angular/angular.js",
+        "./node_modules/angular-sanitize/angular-sanitize.js",
         "./node_modules/@uirouter/angularjs/release/angular-ui-router.js",
         "./node_modules/angular-mocks/angular-mocks.js",
         "./node_modules/angular-animate/angular-animate.js",
         "./node_modules/angular-touch/angular-touch.js",
         "./node_modules/angular-ui-router-anim-in-out/anim-in-out.js",
         "./node_modules/angular-ui-bootstrap/dist/ui-bootstrap-tpls.js",
-        "./node_modules/angular-ui-bootstrap/dist/ui-bootstrap.js"
+        './node_modules/moment/moment.js',
+        './node_modules/angular-momentjs/angular-momentjs.js'
     ],
     "css": [
         "./node_modules/bootstrap/dist/css/bootstrap.css",
